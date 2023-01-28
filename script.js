7d5d27d6f331bc04e558a3d99ff806ae3ddf4bac
IchirouTakashima <ichiroutakashimakun@gmail.com>
2023-01-28 20:30:55 +0800

    Updates

--- script.js
+++ script.js
@@ -2,21 +2,51 @@ const ACCORDION_ITEM_HEADER = document.querySelectorAll(
   ".accordion-item-header"
 )
 
+// ACCORDION_ITEM_HEADER.forEach((header) => {
+//   const ARROW_ICON = header.querySelector("#arrow-icon")
+
+//   header.addEventListener("click", (e) => {
+//     e.preventDefault()
+//     header.classList.toggle("active")
+//     const ACCORDION_ITEM_BODY = header.nextElementSibling
+
+//     if (header.classList.contains("active")) {
+//       ACCORDION_ITEM_BODY.style.maxHeight =
+//         ACCORDION_ITEM_BODY.scrollHeight + "px"
+//       ARROW_ICON.style.transform = "scaleY(-1)"
+//     } else {
+//       ACCORDION_ITEM_BODY.style.maxHeight = 0
+//       ARROW_ICON.style.transform = "scaleY(1)"
+//     }
+//   })
+// })
+
 ACCORDION_ITEM_HEADER.forEach((header) => {
   const ARROW_ICON = header.querySelector("#arrow-icon")
 
   header.addEventListener("click", (e) => {
     e.preventDefault()
-    header.classList.toggle("active")
-    const ACCORDION_ITEM_BODY = header.nextElementSibling
-
+    // Close all accordion items before opening the one that was clicked on
     if (header.classList.contains("active")) {
+      header.classList.remove("active")
+      const ACCORDION_ITEM_BODY = header.nextElementSibling
+      ACCORDION_ITEM_BODY.style.maxHeight = 0
+      ARROW_ICON.style.transform = "scaleY(1)"
+    } else {
+      ACCORDION_ITEM_HEADER.forEach((header) => {
+        header.classList.remove("active")
+        const ACCORDION_ITEM_BODY = header.nextElementSibling
+        ACCORDION_ITEM_BODY.style.maxHeight = 0
+        ARROW_ICON.style.transform = "scaleY(1)"
+      })
+
+      // Open the clicked on accordion item
+      header.classList.add("active")
+      const ACCORDION_ITEM_BODY = header.nextElementSibling
       ACCORDION_ITEM_BODY.style.maxHeight =
         ACCORDION_ITEM_BODY.scrollHeight + "px"
       ARROW_ICON.style.transform = "scaleY(-1)"
-    } else {
-      ACCORDION_ITEM_BODY.style.maxHeight = 0
-      ARROW_ICON.style.transform = "scaleY(1)"
+      //
     }
   })
 })
