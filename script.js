d097b28d3d9f6029f0acd1904617460bb67e1674
IchirouTakashima <ichiroutakashimakun@gmail.com>
2023-01-29 16:59:24 +0800

    Updated file changes

--- script.js
+++ script.js
@@ -2,25 +2,6 @@ const ACCORDION_ITEM_HEADER = document.querySelectorAll(
   ".accordion-item-header"
 )
 
-// ACCORDION_ITEM_HEADER.forEach((header) => {
-//   const ARROW_ICON = header.querySelector("#arrow-icon")
-
-//   header.addEventListener("click", (e) => {
-//     e.preventDefault()
-//     header.classList.toggle("active")
-//     const ACCORDION_ITEM_BODY = header.nextElementSibling
-
-//     if (header.classList.contains("active")) {
-//       ACCORDION_ITEM_BODY.style.maxHeight =
-//         ACCORDION_ITEM_BODY.scrollHeight + "px"
-//       ARROW_ICON.style.transform = "scaleY(-1)"
-//     } else {
-//       ACCORDION_ITEM_BODY.style.maxHeight = 0
-//       ARROW_ICON.style.transform = "scaleY(1)"
-//     }
-//   })
-// })
-
 ACCORDION_ITEM_HEADER.forEach((header) => {
   const ARROW_ICON = header.querySelector("#arrow-icon")
 
@@ -37,7 +18,7 @@ ACCORDION_ITEM_HEADER.forEach((header) => {
         header.classList.remove("active")
         const ACCORDION_ITEM_BODY = header.nextElementSibling
         ACCORDION_ITEM_BODY.style.maxHeight = 0
-        ARROW_ICON.style.transform = "scaleY(1)"
+        header.querySelector("#arrow-icon").style.transform = "scaleY(1)"
       })
 
       // Open the clicked on accordion item
@@ -46,7 +27,6 @@ ACCORDION_ITEM_HEADER.forEach((header) => {
       ACCORDION_ITEM_BODY.style.maxHeight =
         ACCORDION_ITEM_BODY.scrollHeight + "px"
       ARROW_ICON.style.transform = "scaleY(-1)"
-      //
     }
   })
 })
